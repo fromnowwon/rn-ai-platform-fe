96d63ab1114d2e1b2d97c7fb04325b69a502f9c4
Chaewon Jeong <fromnowwon@gmail.com>
2025-08-25 20:23:05 +0900

    feat: 로그인 안 된 경우 로그인 페이지로 리다이렉트 되도록 함

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,29 @@
-import { View, Text } from "react-native";
+import { SafeAreaView, Text, Button } from "react-native";
+import { useRouter } from "expo-router";
+import { useAuthStore } from "@/store/authStore";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (mounted && !isLoggedIn) {
+      router.replace("/login");
+    }
+  }, [mounted, isLoggedIn, router]);
+
+  if (!isLoggedIn) return null;
+
   return (
-    <View className="flex-1 justify-center items-center">
+    <SafeAreaView className="flex-1 justify-center items-center">
       <Text className="text-lg font-bold">Hello World</Text>
-    </View>
+      <Button title="로그인하기" onPress={() => router.push("/login")} />
+    </SafeAreaView>
   );
 }
