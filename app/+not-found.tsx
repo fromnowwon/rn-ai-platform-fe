b02904a59104a7711026c8e4d6d3a3f0ec3af2d7
Chaewon Jeong <fromnowwon@gmail.com>
2025-08-25 16:53:03 +0900

    feat: 초기 설정 및 nativewind 설치

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,10 +1,11 @@
 import { Stack } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function NotFoundScreen() {
   return (
-    <>
+    <View className="flex-1 justify-center items-center">
       <Stack.Screen options={{ title: "Oops!" }} />
-      This screen does not exist.
-    </>
+      <Text>This screen does not exist.</Text>
+    </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,11 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+import { Slot } from "expo-router";
+import { View } from "react-native";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View className="flex-1 bg-white">
+      <Slot />
+    </View>
   );
 }
